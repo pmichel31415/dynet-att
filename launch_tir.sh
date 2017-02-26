680ce70127ad21395dd28327f1ef2485cad724a9
Paul Michel <pmichel1@tir.lti.cs.cmu.edu>
2017-02-25 22:54:23 -0500

    Fixes

--- launch_tir.sh
+++ launch_tir.sh
@@ -4,7 +4,7 @@
 sbatch --gres=gpu:1 --mem=15000 -J medium_model_softmax -o log_medium_model_softmax.txt --time=2-12 run_medium_model_dropout_softmax.sh
 
 # Bidir
-sbatch --gres=gpu:1 --mem=15000 -J medium_model_softmax -o log_medium_model_softmax.txt --time=2-12 run_medium_model_dropout_softmax.sh
+sbatch --gres=gpu:1 --mem=15000 -J medium_model_bidir_softmax -o log_medium_model_bidir_softmax.txt --time=2-12 run_medium_model_dropout_bidir_softmax.sh
 
 # Bidir + word embs
-sbatch --gres=gpu:1 --mem=15000 -J medium_model_softmax -o log_medium_model_softmax.txt --time=2-12 run_medium_model_dropout_softmax.sh
+sbatch --gres=gpu:1 --mem=15000 -J medium_model_bidir_wembs_softmax -o log_medium_model_bidir_wembs_softmax.txt --time=2-12 run_medium_model_dropout_bidir_wembs_softmax.sh
