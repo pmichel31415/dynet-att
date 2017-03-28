d662b5f59ecde514b837e64857722257c5214888
Paul Michel <pmichel31415@gmail.com>
2017-03-28 18:25:34 -0400

    Scripts

---
+++ scripts/run_medium_model_dropout_bidir_softmax.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ~/.bashrc
+NAME=medium_model_dropout_bidir_softmax_nobatch
+# Run big model with attention and dropout
+python train.py --dynet-mem 12000 --dynet-gpus 1 -v -de 512 -dh 512 --batch_size 1 --train_src en-de/train.en-de.low.filt.de --train_dst en-de/train.en-de.low.filt.en --valid_src en-de/valid.en-de.low.de --test_src en-de/test.en-de.low.de --valid_dst en-de/valid.en-de.low.en -en $NAME --test_every 5000 --check_valid_error_every 5000 --check_train_error_every 100 -ml 80 -dr 0.2 --num_epochs 30 --test_out results/${NAME}_test.de-en.en --learning_rate_decay 0.01 --beam_size 5 -bid --train
+
+echo "Done training!"
+
+# Run big model with attention and dropout
+for i in 4;
+    do
+    python train.py --dynet-mem 12000 --dynet-gpus 1 -v -de 512 -dh 512 --batch_size 32 --train_src en-de/train.en-de.low.filt.de --train_dst en-de/train.en-de.low.filt.en --valid_src en-de/valid.en-de.low.de --test_src en-de/blind.en-de.low.de --valid_dst en-de/valid.en-de.low.en -en ${NAME}_beam_$i --test_every 5000 --check_valid_error_every 5000 --check_train_error_every 100 -ml 80 -dr 0.2 --num_epochs 50 --test_out results/${NAME}_beam_${i}_blind.de-en.en --beam_size $i -bid --test -m ${NAME}_model.txt
+done
+echo "Done testing!"
