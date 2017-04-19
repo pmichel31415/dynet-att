7583da031d095d557cc060301919c12c93cade5e
Paul Michel <pmichel31415@gmail.com>
2017-04-19 12:27:22 -0400

    Fix dynet params bug

--- scripts/run_medium_model_dropout_bidir_adam.sh
+++ scripts/run_medium_model_dropout_bidir_adam.sh
@@ -2,10 +2,10 @@
 source ~/.bashrc
 NAME=medium_model_dropout_bidir_sgd
 # Run big model with attention and dropout
-python run.py -c config/best_config.yaml -e train
+python run.py --dynet-seed 3416888402 --dynet-gpus 1 --dynet-mem 512 -c config/best_config.yaml -e train
 
 echo "Done training!"
 
 # Run big model with attention and dropout
-python run.py -c config/best_config.yaml -e test
+python run.py --dynet-seed 3416888402 --dynet-gpus 1 --dynet-mem 512 -c config/best_config.yaml -e test
 echo "Done testing!"
