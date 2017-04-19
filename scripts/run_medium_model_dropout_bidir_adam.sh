e50175bd2a9738ba60a0ada83413cf32cad9e36b
Paul Michel <pmichel31415@gmail.com>
2017-04-19 12:16:25 -0400

    Modify script to take yaml config into account

--- scripts/run_medium_model_dropout_bidir_adam.sh
+++ scripts/run_medium_model_dropout_bidir_adam.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 source ~/.bashrc
-NAME=medium_model_dropout_bidir_adam
+NAME=medium_model_dropout_bidir_sgd
 # Run big model with attention and dropout
-python train.py --dynet-mem 512 --dynet-gpus 1 -v -de 512 -dh 512 -da 256 --batch_size 32 --train_src en-de/train.en-de.low.filt.de --train_dst en-de/train.en-de.low.filt.en --valid_src en-de/valid.en-de.low.de --test_src en-de/valid.en-de.low.de --test_dst en-de/valid.en-de.low.en --valid_dst en-de/valid.en-de.low.en -en $NAME --test_every 2500 --check_valid_error_every 2500 --check_train_error_every 100 -ml 80 -dr 0.5 --num_epochs 10 --test_out results/${NAME}_valid.de-en.en --learning_rate 0.001 --beam_size 3 -bid --train
+python run.py -c config/best_config.yaml -e train
 
 echo "Done training!"
 
 # Run big model with attention and dropout
-for i in 1 2 3 4 5;
-    do
-    python train.py --dynet-mem 512 --dynet-gpus 1 -v -de 512 -dh 512 --batch_size 32 --train_src en-de/train.en-de.low.filt.de --train_dst en-de/train.en-de.low.filt.en --valid_src en-de/valid.en-de.low.de --test_src en-de/test.en-de.low.de --valid_dst en-de/valid.en-de.low.en -en ${NAME}_beam_$i --test_every 2500 --check_valid_error_every 2500 --check_train_error_every 100 -ml 80 -dr 0.0 --num_epochs 50 --test_out results/${NAME}_beam_${i}_test.de-en.en --beam_size $i -bid --test -m ${NAME}_model.txt
-done
+python run.py -c config/best_config.yaml -e test
 echo "Done testing!"
