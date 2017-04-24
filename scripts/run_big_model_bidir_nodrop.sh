2f5462080ca6a712275c5029726bfaa2896e4efa
Paul Michel <pmichel31415@gmail.com>
2017-04-24 17:17:30 -0400

    Clean up scripts

--- scripts/run_big_model_bidir_nodrop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source ~/.bashrc
-NAME=big_model_bidir_nodrop
-# Run big model with attention and dropout
-python train.py --dynet-mem 10000 --dynet-gpus 1 -v -de 1000 -dh 1000 --batch_size 10 --train_src en-de/train.en-de.low.filt.de --train_dst en-de/train.en-de.low.filt.en --valid_src en-de/valid.en-de.low.de --test_src en-de/test.en-de.low.de --valid_dst en-de/valid.en-de.low.en -en $NAME --test_every 1000 --check_valid_error_every 1000 --check_train_error_every 100 -ml 80 -dr 0.0 --num_epochs 10 --test_out results/${NAME}_test.de-en.en --learning_rate_decay 0.1 --beam_size 1 -att -bid --train
-
-echo "Done!"
--- scripts/run_small_model.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ~/.bashrc
-# Run big model with attention and dropout
-python train.py --dynet-mem 10000 --dynet-gpus 1 -v -de 256 -dh 256 --batch_size 10 --train_src en-de/train.en-de.low.filt.de --train_dst en-de/train.en-de.low.filt.en --valid_src en-de/valid.en-de.low.de --test_src en-de/test.en-de.low.de --valid_dst en-de/valid.en-de.low.en -att -en smal_model_dropout --test_every 1000 --check_valid_error_every 1000 --check_train_error_every 100 -ml 80 -dr 0.5 --num_epochs 10
-
-echo "Done!"
