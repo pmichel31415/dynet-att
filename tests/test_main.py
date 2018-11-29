12369f00c24b4588384e10669b5f2caba15ff046
Paul Michel <pmichel31415@gmail.com>
2018-11-29 11:00:25 -0500

    Tests

---
+++ tests/test_main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import unittest
+from unittest import TestCase
+
+import os.path
+import tempfile
+import shutil
+import sys
+HERE = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(HERE, ".."))
+from dynet_mt import main  # noqa
+
+
+class TestMain(TestCase):
+
+    def setUp(self):
+        self.actual_sysargs = sys.argv[:]
+        sys.argv = [sys.argv[0]]
+        self.path = tempfile.mkdtemp()
+        self.data_folder = os.path.join(HERE, "data")
+        self.config_folder = os.path.join(HERE, "config")
+
+    def tearDown(self):
+        shutil.rmtree(self.path)
+        sys.argv = self.actual_sysargs[:]
+
+    def test_train(self):
+        train_src = os.path.join(self.data_folder, "train.ja")
+        train_tgt = os.path.join(self.data_folder, "train.en")
+        valid_src = os.path.join(self.data_folder, "valid.ja")
+        valid_tgt = os.path.join(self.data_folder, "valid.en")
+        sys.argv.extend([
+            # General args
+            "--task", "train",
+            "--exp-name", "test",
+            "--output-dir", self.path,
+            "--verbose",
+            # Tokenizer args
+            "--tokenizer-type", "subword",
+            "--subword-algo", "unigram",
+            "--subword-train-files", train_src, train_tgt,
+            "--subword-voc-size", "1000",
+            # Data args
+            "--train-src", train_src,
+            "--train-tgt", train_tgt,
+            "--valid-src", valid_src,
+            "--valid-tgt", valid_tgt,
+            # Model args
+            "--architecture", "test_bilstm",
+            # Optimization args
+            "--batch-size", "2",
+            "--valid-batch-size", "2",
+        ])
+        main.main()
+
+    def test_train_config_file(self):
+        config_file = os.path.join(self.config_folder, "test_config.yaml")
+        sys.argv.extend([
+            # General args
+            "--config-file", config_file,
+            "--env", "train",
+        ])
+        main.main()
+
+
+if __name__ == '__main__':
+    unittest.main()
